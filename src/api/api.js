27d358ca660d08ba76e062c4f86ab912bdfc5772
Kamil Nahotko <kamilnahotko@gmail.com>
2021-06-08 14:14:38 +0200

    Add handling refresh token

--- src/api/api.js
+++ src/api/api.js
@@ -15,26 +15,29 @@ axios.interceptors.request.use(
   }
 );
 
-// axios.interceptors.response.use(
-//   (response) => {
-//     return response;
-//   },
-//   function (error) {
-//     const originalRequest = error.config;
-//     let refreshToken = localStorage.getItem('refreshToken');
-//     if (refreshToken && error.response.status === 401 && !originalRequest._retry) {
-//       originalRequest._retry = true;
-//       return axios.post(`${baseUrl}/jwt/refresh`, { refreshToken: refreshToken }).then((res) => {
-//         if (res.status === 200) {
-//           localStorage.setItem('accessToken', res.data.accessToken);
-//           console.log('Access token refreshed!');
-//           return axios(originalRequest);
-//         }
-//       });
-//     }
-//     return Promise.reject(error);
-//   }
-// );
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  function (error) {
+    const originalRequest = error.config;
+    let refreshToken = localStorage.getItem('refreshToken');
+    let accessToken = localStorage.getItem('accessToken');
+    if (refreshToken && error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+      return axios
+        .post(`${baseUrl}/jwt/refresh`, { accessToken: accessToken, refreshToken: refreshToken })
+        .then((res) => {
+          if (res.status === 200) {
+            localStorage.setItem('accessToken', res.data.accessToken);
+            localStorage.setItem('refreshToken', res.data.refreshToken);
+            return axios(originalRequest);
+          }
+        });
+    }
+    return Promise.reject(error);
+  }
+);
 
 const api = {
   signup: (body) => {
